bead7df67b4b7c7e724b90257567be9fb36da09e
wildchamo <wildchamo@gmail.com>
2023-03-27 01:26:13 -0500

    agregar estilos de forma nativa a los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { Container } from "@mui/system";
 
 function App() {
-  return <Container>
-    
-  </Container>;
+  return (
+    <Container
+      sx={{
+        background: "whitesmoke",
+        width: "80vw",
+        height: "500px",
+        borderRadius: "16px",
+        marginTop: "40px",
+        display:'flex',
+        flexDirection:"column",
+        alignItems:'center'
+      }}
+    >
+      holaa, soy Jose
+    </Container>
+  );
 }
 
 export default App;
